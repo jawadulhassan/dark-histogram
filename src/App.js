9e08e57910ff9ca7c8d0c64e7bb37961e53f23c5
Jawad Ul Hassan <jawadulhassan50@gmail.com>
2020-03-23 14:21:59 +0500

    major update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts/highcharts.src.js";
+import HighchartsExporting from "highcharts/modules/exporting";
+import darkUnica from "highcharts/themes/dark-unica";
+
+darkUnica(Highcharts);
+
+if (typeof Highcharts === "object") {
+  HighchartsExporting(Highcharts);
+}
+
+var theme = {
+  chart: {
+    backgroundColor: "#141d28"
+  }
+};
+
+Highcharts.setOptions(theme);
+
+function Histogram(props) {
+  const chartOptions = {
+    chart: {
+      type: "column"
+    },
+    title: {
+      text: "Column chart with negative values"
+    },
+    xAxis: {
+      categories: ["Apples", "Oranges", "Pears", "Grapes", "Bananas"]
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: "John",
+        data: [5, 3, 4, 7, 2]
+      },
+      {
+        name: "Jane",
+        data: [2, -2, -3, 2, 1]
+      },
+      {
+        name: "Joe",
+        data: [3, 4, 4, -2, 5]
+      }
+    ]
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </div>
   );
 }
 
-export default App;
+export default Histogram;
